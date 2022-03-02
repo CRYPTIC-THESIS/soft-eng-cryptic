6fce9e914c2b6b4b474c98ea1f4d467fad5b1747
ArcsOriginalVisual <krstnarconado@gmail.com>
2022-03-02 14:24:47 +0800

    changed date until limit

--- admin/modules/app_functions.py
+++ admin/modules/app_functions.py
@@ -163,7 +163,7 @@ class AppFunctions(MainWindow):
 
         # DATE
         self.ui.trainFromDateEdit.setDate(QDate(2020, 1, 1))
-        self.ui.trainUntilDateEdit.setDate(QDate.currentDate())
+        self.ui.trainUntilDateEdit.setDate(QDate(2021, 10, 31))
         self.dataset_date_from = None
         self.dataset_date_until = None
 
